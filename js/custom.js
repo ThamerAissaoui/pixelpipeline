9871ea85bf715d9cbf6e364ffe403c5151e7230f
thameur.aissaoui <thameur.aissaoui@gmail.com>
2025-03-16 20:20:07 -0400

    2nd commit

--- js/custom.js
+++ js/custom.js
@@ -6,41 +6,48 @@ Author:         GraphBerry
 URL:            http://graphberry.com
 License:        http://graphberry.com/pages/license
 -------------------------------------------------------------------*/
-$(function(){
-	'use strict';
 
-/*--------------------------------------------------
-    Initialize portfolio filter
-    ---------------------------------------------------*/
-	$('#portfolio-grid').mixItUp();
+$(document).ready(function() {
+    console.log("Custom script loaded successfully!");
+    // Your other scripts go inside this function
 
-/*--------------------------------------------------
-    Open video modal
-    ---------------------------------------------------*/
-	$('#popup-youtube').magnificPopup({
-	    disableOn: 700,
-	    type: 'iframe',
-	    mainClass: 'mfp-fade',
-	    removalDelay: 160,
-	    preloader: false,
-	    fixedContentPos: false
-	});
-	
-/*--------------------------------------------------
-    Page Scroll Features 
-    ---------------------------------------------------*/
-	smoothScroll.init({
-        speed: 1000,
-        updateURL: true,
-        offset: 50
+    $(function(){
+        'use strict';
+    
+    /*--------------------------------------------------
+        Initialize portfolio filter
+        ---------------------------------------------------*/
+        $('#portfolio-grid').mixItUp();
+    
+    /*--------------------------------------------------
+        Open video modal
+        ---------------------------------------------------*/
+        $('#popup-youtube').magnificPopup({
+            disableOn: 700,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: false,
+            fixedContentPos: false
+        });
+        
+    /*--------------------------------------------------
+        Page Scroll Features 
+        ---------------------------------------------------*/
+        smoothScroll.init({
+            speed: 1000,
+            updateURL: true,
+            offset: 50
+        });
+    
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 130) {
+                $('#header-nav').addClass('navbar-wooster');
+            } else {
+                $('#header-nav').removeClass('navbar-wooster');
+            }
+        });
+    
     });
-
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 130) {
-            $('#header-nav').addClass('navbar-wooster');
-        } else {
-            $('#header-nav').removeClass('navbar-wooster');
-        }
-    });
-
 });
+
